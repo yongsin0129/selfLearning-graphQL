7be3b4be60cff3125a93382209e53ed255a17a93
yongsin0129 <yongsin0129@gmail.com>
2022-12-08 14:53:54 +0800

    feat : 進階 Query 需要配合 schema 的 1.Arguments 2.Variables
    單純 Query skill
    1.Operation Name
    2.Aliases
    3.Fragment

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -15,6 +15,34 @@ const typeDefs = gql`
     age: Int
     "朋友們"
     friends: [User]
+    "身高 (預設為 CENTIMETRE)"
+    height(unit: HeightUnit = CENTIMETRE): Float
+    "體重 (預設為 KILOGRAM)"
+    weight(unit: WeightUnit = KILOGRAM): Float
+  }
+
+  """
+  高度單位
+  """
+  enum HeightUnit {
+    "公尺"
+    METRE
+    "公分"
+    CENTIMETRE
+    "英尺 (1 英尺 = 30.48 公分)"
+    FOOT
+  }
+
+  """
+  重量單位
+  """
+  enum WeightUnit {
+    "公斤"
+    KILOGRAM
+    "公克"
+    GRAM
+    "磅 (1 磅 = 0.45359237 公斤)"
+    POUND
   }
 
   type Query {
@@ -24,6 +52,8 @@ const typeDefs = gql`
     me: User
     "取得所有使用者"
     users: [User]
+    "取得特定 user (name 為必填)"
+    user(name: String!): User
   }
 `
 export default typeDefs
