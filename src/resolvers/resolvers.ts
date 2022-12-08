982fee66da69eff6e271895f3e7d2ae5c5f9d5a9
yongsin0129 <yongsin0129@gmail.com>
2022-12-08 11:06:52 +0800

    feat : using fake data implement Schema and Resolver

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,7 +1,28 @@
- // Resolvers 是一個會對照 Schema 中 field 的 function map ，讓你可以計算並回傳資料給 GraphQL Server
-export const resolvers = {
+export { resolvers }
+// Resolvers 是一個會對照 Schema 中 field 的 function map ，讓你可以計算並回傳資料給 GraphQL Server
+const resolvers = {
   Query: {
-    // 需注意名稱一定要對到 Schema 中 field 的名稱
-    hello: () => 'world'
+    hello: () => 'world',
+    // 3. 加上 me 的 resolver (一定要在 Query 中喔)
+    me: () => users[0]
   }
 }
+
+// 假資料
+const users = [
+  {
+    id: 1,
+    name: 'Fong',
+    age: 23
+  },
+  {
+    id: 2,
+    name: 'Kevin',
+    age: 40
+  },
+  {
+    id: 3,
+    name: 'Mary',
+    age: 18
+  }
+]
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,12 +1,25 @@
 import { gql } from 'apollo-server'
 // gql: template literal tag, 讓你在 Javascript 中使用 GraphQL 語法
 
-
 // GraphQL Schema 定義
 const typeDefs = gql`
+  """
+  使用者資訊
+  """
+  type User {
+    "識別碼"
+    id: ID
+    "名字"
+    name: String
+    "年齡"
+    age: Int
+  }
+
   type Query {
     "A simple type for getting started!"
     hello: String
+    "取得當下使用者"
+    me: User
   }
 `
 export default typeDefs
