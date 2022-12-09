8d4bd84182b0d48410e3aea238927e5bc9ebaaea
yongsin0129 <yongsin0129@gmail.com>
2022-12-09 20:34:38 +0800

    feat : apollo v4 migrate

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,40 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from '@apollo/server'
 // ApolloServer: 讓我們啟動 server 的 class ，不但實作許多 GraphQL 功能也提供 web application 的功能 (背後使用 express)
+import { startStandaloneServer } from '@apollo/server/standalone'
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault
+} from '@apollo/server/plugin/landingPage/default'
 
 import typeDefs from './schema'
 import { resolvers } from './resolvers'
 
+interface MyContext {
+  token?: String
+}
+
 // 初始化 Web Server ，需傳入 typeDefs (Schema) 與 resolvers (Resolver)
-const server = new ApolloServer({
+const server = new ApolloServer<MyContext>({
   typeDefs,
   resolvers,
-  cache: 'bounded'
+  persistedQueries: false,
+  plugins: [
+    // Install a landing page plugin based on NODE_ENV
+    process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: 'my-graph-id@my-graph-variant',
+          footer: false,
+          embed:true,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false })
+  ]
 })
+const boost = async () => {
+  const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+    listen: { port: 4000 }
+  })
+  console.log(`🚀  Server ready at ${url}`)
+}
 
-server.listen().then(({ url }) => {
-  console.log(`Server is running on ${url}`)
-})
+boost()
